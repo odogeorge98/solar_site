9d2a39f69b715c7b6cc4c84193212b1c663367e0
odogeorge98 <odogeorge98@gmail.com>
2024-10-16 08:57:19 +0100

    Updated

--- scripts/index.js
+++ scripts/index.js
@@ -38,3 +38,16 @@ function handleScroll() {
 }
 
 window.addEventListener('scroll', handleScroll);
+
+
+
+
+  window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+
